49a1f6f64088270c8c975d3b6b9fdf35e85824f4
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-18 19:21:17 -0500

    Created directories for go programs

---
+++ src/variables.go
@@ -0,0 +1,18 @@
+ package main
+
+ import (
+	 "fmt"
+ )
+
+ func main() {
+	 var i int
+	 i = 42
+	 var j int 
+	 j = 58
+
+	 i := 123
+	 // fmt.Println("Hellow, playground")
+	 // fmt.Println(i)
+	 fmt.Printf("%v, %T,j ,j ") //Prints the value and Type respectively
+ }
+ 
