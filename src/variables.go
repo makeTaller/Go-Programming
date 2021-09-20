59b56b4b81f936ce65069581d690f28578a06d2c
Kirk Tolliver <KirkTolliver28@gmail.com>
2021-09-20 10:22:04 -0500

    Variable declarations in go

--- src/variables.go
+++ src/variables.go
@@ -5,14 +5,20 @@
  )
 
  func main() {
-	 var i int
-	 i = 42
-	 var j int 
-	 j = 58
+	 var  (
+		 		fname = "Jason"
+				lname = "Black"
+				birth = "01/23/54"
+				role = "master teacher"
+			)
+	 var i int = 42
+	 var j int = 58
 
-	 i := 123
+	 // i := 123
 	 // fmt.Println("Hellow, playground")
 	 // fmt.Println(i)
-	 fmt.Printf("%v, %T,j ,j ") //Prints the value and Type respectively
+	 fmt.Printf("%v, %T\n",j ,j ) //Prints the value and Type respectively
+	 fmt.Printf("%v, %T\n",i ,i ) //Prints the value and Type respectively
+	 fmt.Println(fname,lname,birth,role)
  }
  
